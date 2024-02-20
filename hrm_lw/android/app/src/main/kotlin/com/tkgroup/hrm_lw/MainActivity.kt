fdcf35e1df81dffc0d5f4484663df3699fd250da
LITTAVANH PHANALASY <littavanhphanalasy@gmail.com>
2024-02-20 09:03:35 +0700

    commit

---
+++ hrm_lw/android/app/src/main/kotlin/com/tkgroup/hrm_lw/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tkgroup.hrm_lw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- hrm_lw/lib/utils/url_constants.dart
+++ hrm_lw/lib/utils/url_constants.dart
@@ -1,7 +1,7 @@
 class UrlConstants {
   // static const String initURL = "https://ltlhrtest.tkgroup.la:44375/api/";
-// static const String initURL = "http://hrm.tkgroup.la:44375/api/"; real
-  static const String initURL = "http://192.168.100.28:8449/api/"; 
+static const String initURL = "https://hrm.tkgroup.la:44375/api/"; 
+  // static const String initURL = "http://192.168.100.28:8449/api/"; 
   static String MY_LEAVE_REQUEST_LIST_URL = "User/LeaveRequestList";
   static String ADD_MY_LEAVE_REQUEST = "User/NewleaveRequest";
   static String LEAVE_TYPE = "User/LeaveType";
